ffe24b36aec594fe8cee8130e6630c38ec0db3d8
Nicolai <github@vandersmagt.nl>
2022-05-23 14:56:34 +0200

    Add Lacework IAC config

--- s3-2.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "dev_website" {  
-  bucket_prefix = var.bucket_prefix  
-  acl    = "public-read"
-}
--- s3.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "prod_website" {  
-  bucket_prefix = var.bucket_prefix  
-  acl    = "public-read"
-}
